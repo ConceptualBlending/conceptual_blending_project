946e7500308fea06008934d3f090cbadf11093b9
Madhura Thosar <madhura.thosar@gmail.com>
2014-10-15 21:55:39 +0200

    initial commit

---
+++ source/creative_process.rb
@@ -0,0 +1,66 @@
+#!/usr/bin/ruby
+
+# experimental script for finding axiom sets suitable for proofs with SPASS
+require 'open3'
+require 'io/wait'
+require 'evaluation'
+require 'blend_pattern'
+require 'input_space'
+
+
+inputfilename = "blendTigerHorse.dol"
+blendnodename = "evalmb1"
+bkfilename = "background_knowledge.dol"
+requirement = "requirement.dol"
+
+
+
+# methods
+# select blending pattern
+# # Square the number
+
+
+# define class for coinvent project
+def creative_process
+
+# call the blending pattern mathod and receive the pattern for blending,
+# connect to blending pattern repository
+# blending patterns will be a dol file
+# pattern = select_blending_pattern()   
+
+
+# get the input spaces from the repository
+# returns input space in an array. 
+# input_space = select_input_space(pattern, bkfilename, requirement)
+
+# create a base and a base morphism from input space
+# base, base_morphism = creat_base(input_space)
+
+# create blends
+# blendoid = create_blendoid(input_space, base, base_morphism)
+
+# evaluate for consistency
+# evaluate_blend(o, stdin, stdout, inputfilename)
+
+
+	
+
+
+
+
+return final_blendoid
+
+end
+
+
+
+	
+
+	
+
+
+
+
+
+
+
