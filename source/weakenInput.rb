0b10d8871d47da1f4ed2ef40563f1aee1f0cc8f1
Madhura Thosar <madhura.thosar@gmail.com>
2014-11-18 20:06:34 +0100

    old code files deleted

--- source/weakenInput.rb
+++ source/weakenInput.rb
@@ -1,33 +1,8 @@
-# This method weakens the input. It needs to be improved considerably. 
+# ------ This method weakens the input spaces if the blendoid inconsistent
+
+def weaken_input(inputSpace_1, inputSpace_2)
 
 
-def weaken_Input(fname)
-  puts("Weaken")
-  lines = File.read(fname).split("\n")
-  occurNo = File.read(fname).scan("SubClassOf:").count
+  return weakenInput_1, weakenInput_2
 
-  File.open(fname, "w") do |file|
-    lines.each do |line|
-     if (line.scan("SubClassOf:").length == 1) 
-        puts ("step ")
-        rNumber = 1 + rand(occurNo)
-        puts("rn:"+rNumber.to_s)  
-        if (rNumber != occurNo)
-            file.write(line+"\n")
-          else 
-             puts("deleted:"+line)
-          end
-      else
-        file.write(line+"\n")
-     end
-    end
-  end
-
-  occurNo2 = File.read(fname).scan("SubClassOf:").count
-
-  if (occurNo ==  occurNo2) 
-    weaken_Input(fname)
-   else
-    return occurNo
-  end
 end
