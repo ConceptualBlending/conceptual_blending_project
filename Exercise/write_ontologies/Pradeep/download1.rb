9daa4c31e75be629135c345ba65dcfc9f9c37923
mcodescu <mscodescu@gmail.com>
2014-12-10 14:09:11 +0100

    example of repository with dol files

--- Exercise/write_ontologies/Pradeep/download1.rb
+++ Exercise/write_ontologies/Pradeep/download1.rb
@@ -1,13 +1,13 @@
 def generateListOfFiles(path)
-   system("git clone" +path)
+   system("git clone" ++ path ++ "myFolder")
    if Dir[path] == nil
       puts "Entered wrong path"
    end
-   Dir.chdir("conceptual_blending_project/Exercise/write_ontologies")
+   Dir.chdir("myFolder")
 
    puts "\nList of Owl Files :" ,Dir.glob("*.owl")
    puts "\nList of dol Files :" ,Dir.glob("*.dol")
 end
 
 
-generateListOfFiles(" https://github.com/ConceptualBlending/conceptual_blending_project.git")
+generateListOfFiles("git://ontohub.org/dol-examples.git")
