89022ed459851ec9985338a1d31326e612645bd9
pradeep-veera <pradeep.veera@rocketmail.com>
2014-12-12 23:46:38 +0100

    Update selectTwoRandomFiles.rb

--- Exercise/write_ontologies/Pradeep/selectTwoRandomFiles.rb
+++ Exercise/write_ontologies/Pradeep/selectTwoRandomFiles.rb
@@ -1,11 +1,12 @@
 def selectRandomFile(filePath)
       file_dir = Dir.entries(filePath)
-      file_dir.delete_if {|file_name| file_name == ".." or file_name == "." or file_name == " "}
-      2.times do
-         file_size = file_dir.size
-         puts file_dir[rand(1..file_size)]
-      end
+      owl_dir = file_dir.delete_if {|x| File.extname(x) != ".owl"}
+      owl1 = owl_dir[rand(1..(owl_dir.size)-1)]
+      owl_dir2 = owl_dir.delete_if {|x| File.basename(x) == owl1 } # To generate another random file unlike the before one 
+      owl2 = owl_dir2[rand(1..(owl_dir2.size)-1)]
+      puts owl1
+      puts owl2
 end
 
-
 selectRandomFile("/home/pradeep/conceptual_blending_project/Exercise/write_ontologies")
+
