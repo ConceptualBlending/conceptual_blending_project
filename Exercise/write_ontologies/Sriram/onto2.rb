732fe49f72311c446407e071f33f361b3440052f
Sriram <sriram.uthaya@st.ovgu.de>
2014-12-14 17:36:35 +0100

    corrected algorithm

--- Exercise/write_ontologies/Sriram/onto2.rb
+++ Exercise/write_ontologies/Sriram/onto2.rb
@@ -1,36 +1,44 @@
 
+
 load '../../../source/data.rb'
 
 
-   def replace_equivalences(onto)
-      sig = onto.o_signature 
-      sens = onto.o_sentences  # why did you do this if you set it on next line to []?
-      sens =[]
+class Replace
 
-      onto.o_sentences.each do|x|
-         if @o_sentences.select{|z| z.is_a?(ConceptEquivalence(a,b))} # the select method gives you a sublist
-          # you want to make a test about x (2 lines above) being a ConceptEquivalence
-          # look at data.rb, allSubsumption to see how this is done - the argument of is_a is a class name  
-            @components = [a,b] # there is no a or b here
-            a = components(0) # here you want to take the first element from x.components
-            b = components(1)
-            y = ConceptSubsumption(a,b) # assuming a and b were correct, here you create a sentence with new, see examples at the end of data.rb
-            sens.insert(y) # insert will require another argument, the index where you want to insert
-                           # look for a method that inserts at the end of the array!
-         else
-            sens() # this is unfinished code, you want to add x here at the end of sens
-         end
-      end
-      # here you must create a new ontology and return it
+   def initialize(obj)
+      sig = obj.o_signature
    end
 
+   def repConcept(obj)
+      sens = [ ]   
+      obj.o_sentences.each do|x|
+         if x.is_a?(ConceptEquivalence) 
+            @components = x 
+            a = x.components[0] 
+            b = x.components[1]
+            y = ConceptSubsumption.new(a, b) 
+            sens.push(y) 
+            p sens
+         end  
+      end
+   end
+end
+
+ #Class: Mammal SubClassOf: Animal
+
+m = Symbols.new(CLASS, "Mammal")
+m.show
+a = Symbols.new(CLASS, "Animal")
+a.show
+mc = AtomicConcept.new(m)
+ac = AtomicConcept.new(a)
+s1  = ConceptEquivalence.new(mc, ac)
 
 
-#onto = Ontology.new() 
-# you need to create an instance of the Ontology class, that you pass as an argument to your function
-# maybe it's best to create something on your own, to understand the data structures:
-#  three classes, A,B,C
-#  A subclassof B
-#  B equivalent C
-# look at the end of data.rb to see how it is done!
-replace_equivalences(onto)
+cSet1 = Set[a, m]
+oSet1 = Set[]
+dSet1 = Set[]
+iSet1 = Set[]
+
+sigma1 = Signature.new(cSet1,oSet1,dSet1,iSet1)
+onto1 = Ontology.new(sigma1, [s1])
