5567f4a48d5402cd42515a8873b2b3c66a3b9bd2
Sriram <sriram.uthaya@st.ovgu.de>
2014-12-15 14:27:23 +0100

    new instances

--- Exercise/write_ontologies/Sriram/onto2.rb
+++ Exercise/write_ontologies/Sriram/onto2.rb
@@ -1,44 +1,48 @@
 
+load '\..\..\..\source\data.rb'
 
-load '../../../source/data.rb'
-
-
-class Replace
-
-   def initialize(obj)
-      sig = obj.o_signature
-   end
 
    def repConcept(obj)
-      sens = [ ]   
+      sig = obj.o_signature
+	  sens = [ ]   
       obj.o_sentences.each do|x|
          if x.is_a?(ConceptEquivalence) 
-            @components = x 
             a = x.components[0] 
             b = x.components[1]
             y = ConceptSubsumption.new(a, b) 
             sens.push(y) 
-            p sens
+          else 
+		    sens.push()
          end  
       end
    end
-end
-
- #Class: Mammal SubClassOf: Animal
-
-m = Symbols.new(CLASS, "Mammal")
-m.show
-a = Symbols.new(CLASS, "Animal")
-a.show
-mc = AtomicConcept.new(m)
-ac = AtomicConcept.new(a)
-s1  = ConceptEquivalence.new(mc, ac)
 
 
-cSet1 = Set[a, m]
+  
+   
+ #Class: A SubClassOf: B
+
+A= Symbols.new(CLASS, "A")
+B = Symbols.new(CLASS, "B")
+Ac = AtomicConcept.new(A)
+Bc = AtomicConcept.new(B)
+s1  = ConceptSubsumption.new(Ac, Bc)
+
+ #Class: B EquivalentTo: C
+ 
+ C = Symbols.new(CLASS, "C")
+ Cc = AtomicConcept.new(C)
+ s2 = ConceptEquivalence.new(Bc, Cc)
+
+cSet1 = Set[A, B, C]
 oSet1 = Set[]
 dSet1 = Set[]
 iSet1 = Set[]
 
 sigma1 = Signature.new(cSet1,oSet1,dSet1,iSet1)
-onto1 = Ontology.new(sigma1, [s1])
+onto1 = Ontology.new(sigma1, [s1, s2])
+
+
+onto2 = repConcept(onto1)
+
+p onto2
