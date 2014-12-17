c1a46dc11b2f6dcaccad362d0979a48ee18ac850
pradeep-veera <pradeep.veera@rocketmail.com>
2014-12-17 10:57:26 +0100

    added return value for selectTwoRandomFiles.rb program

--- Exercise/write_ontologies/Pradeep/selectTwoRandomFiles.rb
+++ Exercise/write_ontologies/Pradeep/selectTwoRandomFiles.rb
@@ -1,11 +1,13 @@
 def selectRandomFile(filePath)
+      randomFiles = []
       file_dir = Dir.entries(filePath)
       owl_dir = file_dir.delete_if {|x| File.extname(x) != ".owl"}
       owl1 = owl_dir[rand(1..(owl_dir.size)-1)]
       owl_dir2 = owl_dir.delete_if {|x| File.basename(x) == owl1 } # To generate another random file unlike the before one 
       owl2 = owl_dir2[rand(1..(owl_dir2.size)-1)]
-      puts owl1
-      puts owl2
+      randomFiles.push(owl1)
+      randomFiles.push(owl2)
+      return randomFiles
 end
 
 selectRandomFile("/home/pradeep/conceptual_blending_project/Exercise/write_ontologies")
