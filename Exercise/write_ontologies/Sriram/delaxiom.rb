f5228454bf0927e3a97b011fbf69cee5c5821b91
sriram308 <sriram.uthaya@st.ovgu.de>
2015-01-25 00:06:37 +0100

    weakening by random deletion of axioms #22

--- Exercise/write_ontologies/Sriram/delaxiom.rb
+++ Exercise/write_ontologies/Sriram/delaxiom.rb
@@ -1,33 +1,23 @@
 load 'data.rb'
 	
 
+def list(var)
+    random = rand(var.length)
+	var.delete_at(random)
+	return var
+end    
 def deleteaxiom(obj1,obj2)
- 
+    
     sig1 = obj1.o_signature
     sens1 = obj1.o_sentences
-    obj1.o_sentences.each do |k|
-     a = k.components[0]
-    
-     b = k.components[1]
-end
-    x = Ontology.new(sig1, sens1)
-	
+	l1 = list(sens1)
     sig2 = obj2.o_signature
     sens2 = obj2.o_sentences
-    obj1.o_sentences.each do |k|
-     a = k.components[0]
-    
-     b = k.components[1]
-end
-    y = Ontology.new(sig2, sens2)
-    list = [x,y]
-	puts ("list of Axioms")
-    puts list
-    random = rand(list.length)
-   	list.delete_at(random)
-	return list
-    	
- 
+	l2 = list(sens2)
+    ont1 = Ontology.new(sig1,l1)
+    ont2 = Ontology.new(sig2,l2)
+    ar = [ont1,ont2]
+	return ar
 end
 
  #Class: A SubClassOf: B
@@ -40,7 +30,7 @@ s1  = ConceptSubsumption.new(Ac, Bc)
 
  #Class: E SubClassOf: F
 
-E= Symbols.new(CLASS, "E")
+E = Symbols.new(CLASS, "E")
 F = Symbols.new(CLASS, "F")
 Ec = AtomicConcept.new(E)
 Fc = AtomicConcept.new(F)
@@ -64,7 +54,7 @@ onto1 = Ontology.new(sigma1, [s1, s3])
 
  #Class: K SubClassOf: I
 
-K= Symbols.new(CLASS, "K")
+K = Symbols.new(CLASS, "K")
 I = Symbols.new(CLASS, "I")
 Kc = AtomicConcept.new(K)
 Ic = AtomicConcept.new(I)
@@ -79,6 +69,6 @@ sigma2 = Signature.new(cSet2,oSet2,dSet2,iSet2)
 onto2 = Ontology.new(sigma2, [s2, s4])
 
 del = deleteaxiom(onto1,onto2)
-puts("Resulting Axiom after deleting a random element")
+puts("Selected Axioms")
 puts del
 
