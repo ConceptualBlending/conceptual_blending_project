e412f0c54a9faf2e9936367d1ffe70f141a0735b
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2016-02-20 18:57:42 +0100

    Add possibility to select a prover.

--- script/inconsistency_check.rb
+++ script/inconsistency_check.rb
@@ -1,6 +1,7 @@
 require 'tempfile'
 require 'rest-client'
 require_relative 'hets_basics.rb'
+require_relative 'user_interaction.rb'
 
 class InconsistencyCheck
   include HetsBasics
@@ -20,15 +21,19 @@ HET
                   includeProof: 'false',
                   includeDetails: 'false'}
 
-  attr_accessor :theory_url, :result
+  attr_accessor :theory_url, :provers, :prover, :result
 
   def initialize(theory_url)
     self.theory_url = theory_url
   end
 
   def run
-    try_until_limit_reached_or_solved(limit: MAX_TRIES) do |timeout|
-      check_inconsistency(timeout)
+    with_tempfile do |filepath|
+      retrieve_provers(filepath)
+      select_prover
+      try_until_limit_reached_or_solved(limit: MAX_TRIES) do |timeout|
+        check_inconsistency(filepath, timeout)
+      end
     end
 
     if theory_inconsistent?
@@ -50,12 +55,22 @@ HET
     ->(try_count) { BASE_TIMEOUT * try_count }
   end
 
-  def check_inconsistency(timeout)
-    with_tempfile do |filepath|
-      self.result = call_hets_api(:post,
-                                  hets_action_url(filepath),
-                                  request_data(timeout))
-    end
+  def retrieve_provers(filepath)
+    self.provers =
+      call_hets_api(:get, hets_action_url_provers(filepath))['provers']
+  end
+
+  def select_prover
+    index = UserInteraction.
+      new("Please select a prover for the inconsistency check.",
+          provers.map { |cc| cc['name'] }).run
+    self.prover = provers[index]['identifier']
+  end
+
+  def check_inconsistency(filepath, timeout)
+    self.result = call_hets_api(:post,
+                                hets_action_url_prove(filepath),
+                                request_data(timeout))
   end
 
   def with_tempfile
@@ -67,14 +82,20 @@ HET
   end
 
   def request_data(timeout)
-    REQUEST_DATA.merge(timeout: timeout.to_s).to_json
+    REQUEST_DATA.merge(timeout: timeout.to_s,
+                      prover: prover).to_json
   end
 
   def file_content
     FILE_CONTENT.sub('URL', theory_url)
   end
 
-  def hets_action_url(filepath)
+  def hets_action_url_provers(filepath)
+    url = "file://#{filepath}"
+    "#{HetsBasics::HETS_URL}/provers/#{escape(url)}/auto?format=json"
+  end
+
+  def hets_action_url_prove(filepath)
     url = "file://#{filepath}"
     "#{HetsBasics::HETS_URL}/prove/#{escape(url)}/auto"
   end
